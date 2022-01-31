a524d781d86b8e20362be0092bd3d2729e2ad395
ShashankBargali <shashankbargali@gmail.com>
2022-01-31 15:08:52 +0530

    Bar Bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,17 +8,17 @@ export default function Navbar(prop) {
     <div>
       <nav className={`navbar navbar-expand-lg navbar-${prop.mode} bg-${prop.mode}`}>
         <div className="container-fluid">
-          <Link className="navbar-brand" to="Text-Utils/">{prop.title}</Link>
+          <Link className="navbar-brand" to="/Text-Utils/">{prop.title}</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="Text-Utils/">Home</Link>
+                <Link className="nav-link active" aria-current="page" to="/Text-Utils/">Home</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="Text-Utils/about">About</Link>
+                <Link className="nav-link" to="/Text-Utils/about">About</Link>
               </li>
             </ul>
             <div className="form-check form-switch">
